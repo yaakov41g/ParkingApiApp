f499dd24e3294b25c52c717c35a49d995c24bae0
Yaakov Whise <yaakov41g.@gmail.com>
2025-10-13 14:48:47 +0300

    Continuing the user flow. Adding docker.

--- Models/City.cs
+++ Models/City.cs
@@ -1,7 +1,14 @@
-﻿namespace ParkingApiApp.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ParkingApiApp.Models
 {
     public class City
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
         public string Name { get; set; } = "";
         //public string Region { get; set; } = "";
         public List<string> Zones { get; set; } = new();
--- Services/CityService.cs
+++ Services/CityService.cs
@@ -1,18 +1,25 @@
-﻿using System.Text.Json;
-using MongoDB.Driver;
-using StackExchange.Redis;
+﻿using MongoDB.Driver;
+using ParkingApiApp.Controllers;
 using ParkingApiApp.Models;
-
+using ParkingApiApp.Services;
+using StackExchange.Redis;
+using System.Text.Json;
+using System.Threading.Tasks;
 public class CityService
 {
+    private List<City>? _cachedCities;
+    private readonly TranslationService _translationService;
     private readonly IMongoCollection<City> _cities;
     private readonly IDatabase _cache;
-
-    public CityService(IMongoClient mongoClient, IConnectionMultiplexer redis)
+    private readonly ILogger<CityService> _logger;
+    public CityService(IMongoClient mongoClient, IConnectionMultiplexer redis, TranslationService translationService,
+                ILogger<CityService> logger)
     {
         var db = mongoClient.GetDatabase("ParkingDB");
         _cities = db.GetCollection<City>("cities");
         _cache = redis.GetDatabase();
+        _translationService = translationService;   
+        _logger = logger;   
     }
 
     /// <summary>
@@ -22,11 +29,14 @@ public class CityService
     /// </summary>
     public async Task<List<City>> GetCitiesAsync()
     {
-        // Step 1: Try to get cities from Redis
+     // Step 1: Try to get cities from Redis
+        if (_cachedCities != null)
+            return _cachedCities;
         var cached = await _cache.StringGetAsync("cities_full");
         if (cached.HasValue)
         {
-            return JsonSerializer.Deserialize<List<City>>(cached) ?? new List<City>();
+            _cachedCities = JsonSerializer.Deserialize<List<City>>(cached) ?? new List<City>();
+            return _cachedCities;
         }
 
         // Step 2: Try to get cities from MongoDB
@@ -34,7 +44,7 @@ public class CityService
         if (citiesInDb.Count > 0)
         {
             // Cache the result in Redis
-            await _cache.StringSetAsync("cities_full", JsonSerializer.Serialize(citiesInDb), TimeSpan.FromHours(1));
+            await _cache.StringSetAsync("cities_full", JsonSerializer.Serialize(citiesInDb), null);
             return citiesInDb;
         }
 
@@ -45,10 +55,23 @@ public class CityService
         await _cities.InsertManyAsync(citiesFromJson);
 
         // Cache in Redis
-        await _cache.StringSetAsync("cities_full", JsonSerializer.Serialize(citiesFromJson), TimeSpan.FromHours(1));
+        await _cache.StringSetAsync("cities_full", JsonSerializer.Serialize(citiesFromJson), null);
 
         return citiesFromJson;
     }
+    public async Task<City?> FindCity(string inputName)
+    {
+        _cachedCities = await GetCitiesAsync();
+        _logger.LogInformation($"###### CachedCities  "+ _cachedCities); 
+
+        if (_cachedCities == null || !_cachedCities.Any())
+            return null; // Or throw, or load from Redis if needed
+
+        string englishName = await _translationService.TranslateHebrewToEnglishAsync(inputName);
+        return _cachedCities.FirstOrDefault(c =>
+            c.Name.Equals(englishName, StringComparison.OrdinalIgnoreCase));
+    }
+
 
     /// <summary>
     /// Loads full city data from a local JSON file.
